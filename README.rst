947cd460b4976a473d9c15aa2df659375c64c2e9
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-12-13 15:49:57 +0900

    first commit

---
+++ README.rst
@@ -0,0 +1,13 @@
+===========
+sll.locales
+===========
+
+sll.locales overrides default translations of Plone for SLT site.
+
+Change log
+----------
+
+0.1 (2012-12-xx)
+================
+
+- Initial release. [taito]
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='sll.locales',
+    version='0.0',
+    description="Overrides default translations of Plone for SLT site.",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"],
+    keywords='',
+    author='Taito Horiuchi',
+    author_email='taito.horiuchi@abita.fi',
+    url='https://github.com/taito/slt.locales',
+    license='None-free',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['sll'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Products.CMFPlone',
+        'setuptools'],
+    entry_points="""
+    # -*- Entry points: -*-
+
+    [z3c.autoinclude.plugin]
+    target = plone
+    """)

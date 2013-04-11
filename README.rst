ce7782440c33a1f2651e56f9721173dc1c6d1847
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-04-11 13:32:33 +0300

    Release 0.3.1

--- README.rst
+++ README.rst
@@ -7,6 +7,11 @@ slt.locales overrides default translations of Plone for SLT site.
 Change log
 ----------
 
+0.3.1 (2013-04-11)
+==================
+
+- The previous release did not go OK, so releasing again. [taito]
+
 0.3 (2013-04-11)
 ================
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='slt.locales',
-    version='0.3',
+    version='0.3.1',
     description="Overrides default translations of Plone for SLT site.",
     long_description=open("README.rst").read(),
     classifiers=[
